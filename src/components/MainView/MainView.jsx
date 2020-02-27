e5c94b90bfbb40c2416889303a62454f99bbf1b7
johnray-martinez <jrvmartinez@icloud.com>
2020-02-27 16:09:28 -0500

    initial commit for better-than-the-weather

---
+++ src/components/MainView/MainView.jsx
@@ -0,0 +1,14 @@
+// this is where we get our API data from weather, pictures and location
+import React, { Component } from 'react';
+
+// class MainView extends Component {
+//     render(){
+//         coords = navigator.geolocation.getCurrentPosition(position=> {
+//             return {long: position.coords.longitude, lat: position.coords.latitude}
+//         })
+//         console.log(coords);
+//         return (
+//             <p>hi</p>
+//         )
+//     }    
+}
