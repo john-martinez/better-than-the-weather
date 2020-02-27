c31379c8928ccb541df8022ed25cf9d43b8f880b
johnray-martinez <jrvmartinez@icloud.com>
2020-02-27 17:07:24 -0500

    app is now able to fetch data from open weather API and location

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import MainView from './components/MainView/MainView';
 
 function App() {
   return (
     <div className="App">
-      
+      <MainView />
     </div>
   );
 }
--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,10 +1,40 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+const API_KEY = "d6e0f59ad2174a7b9e3abd197d40271a";
+const LINK = "http://api.openweathermap.org/data/2.5/weather";
 
 class MainView extends Component {
-    render(){
-       
+    state = { location: "", weather: "" }
+
+    componentDidMount(){
+        document.title = 'Better Than The Weather App';
+        navigator.geolocation.getCurrentPosition(position=> {
+            let long = position.coords.longitude;
+            let lat = position.coords.latitude;
+            let weather = "";
+            let location = "";
+            axios.get(`${LINK}?lat=${lat}&lon=${long}&appid=${API_KEY}`)
+            .then(data=>{
+                weather = data.data.weather[0].main;
+                location = data.data.name;
+            })
+            .then(data=>this.setState({location, weather}))
+            .catch(console.log)
+        })
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        console.log(this.state.location);
+        console.log(this.state.weather)
+    }
+    render(){   
         return (
-            <p>hi</p>
+            <div>
+                <p>Location: {this.state.location}</p>
+                <p>Weather: {this.state.weather}</p>
+            </div>
         )
     }    
 }
+
+export default MainView;
