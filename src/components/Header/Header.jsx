fe0288a39a14a0fc47e6a5895d09e5266312e3eb
Idilhyder <idilhyder@gmail.com>
2020-02-28 13:14:23 -0500

    header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,14 +3,18 @@ import './Header.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
 import { faNewspaper } from '@fortawesome/free-solid-svg-icons';
-
+import {Link} from 'react-router-dom';
 
 
 function Header(){
     return(
         <ul className="nav">
-            <li className="nav__item">HOME<FontAwesomeIcon icon={faHome} size="6x" className ="icon-style"/></li>
-            <li className="nav__item">TODAY'S NEWS<FontAwesomeIcon icon={faNewspaper} size = "6x" className ="icon-style"/></li>
+            <Link to = "/">
+            <li className="nav__item">HOME<FontAwesomeIcon icon={faHome} size="2x" className ="icon-style"/></li>
+            </Link>
+            <Link to = "/news">
+            <li className="nav__item">TODAY'S NEWS<FontAwesomeIcon icon={faNewspaper} size = "2x" className ="icon-style"/></li>
+            </Link>
         </ul>
     );
 }
